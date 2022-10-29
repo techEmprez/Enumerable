87656c8da418f1793698aaa0c5214746ed405d7f
techEmprez <choiceosobor@gmail.com>
2022-10-29 08:23:30 +0100

    add enumerable of any method

--- enumerable.rb
+++ enumerable.rb
@@ -1,9 +1,13 @@
-# Enumerable of #all? method
-module Enumerable
+# Module Enumerable of #all? method
   def all?
     result = true
-
     each { |item| result = false unless yield item }
+    result
+  end
 
+# Module Enumerable of #any? method
+  def any?
+    result = false
+    each{ |item| result = true if yield item }
     result
   end
